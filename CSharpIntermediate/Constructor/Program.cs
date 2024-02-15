f3cbd10b3bd7c41b501cd1151f12bb8d112a2834
Umut Ozcan <umut.yozcan@gmail.com>
2024-02-15 17:53:18 +0300

    constructors & interfaces

---
+++ CSharpIntermediate/Constructor/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Customer customer = new() // Default Constructor
+{
+    Id = 1,
+    FirstName = "Ahmet",
+    LastName = "Sozen",
+    City = "Istanbul"
+};
+Console.WriteLine(customer.FirstName + " " + customer.LastName);
+
+Console.WriteLine();
+
+Customer customer2 = new(2, "Mehmet", "Yılmaz", "Anakara"); // Parametreli Ctor
+Console.WriteLine(customer2.FirstName + " " + customer2.LastName);
+
+
+Console.ReadKey();
+
+class Customer
+{
+    public Customer()
+    {
+        Console.WriteLine("new() yapıldıgı an ctor calisir");
+    }
+    public Customer(int id, string firstName, string lastName, string city) // ctor
+    {
+        Console.WriteLine("new(id,firstName,lastName,city) yapıldıgı an ctor calisir");
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        City = city;
+    }
+
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string City { get; set; }
+}
---
+++ CSharpIntermediate/Interfaces/Program.cs
@@ -0,0 +1,74 @@
+﻿
+//IPersonManager personManager = new(); Interfaceler newlenemez
+
+//class - inherits ---------- interface - implements
+
+CustomerManager customerManager = new();
+customerManager.Add();
+
+EmployeeManager employeeManager = new();
+employeeManager.Add();
+
+Console.WriteLine();
+
+// Project Managerda interface ile parametre istediğimizden iki farklı classı interface sayesinde aynı parametre olarak kullanabiliriz
+ProjectManager projectManager = new(); 
+projectManager.Add(new EmployeeManager());
+projectManager.Add(new CustomerManager());
+
+
+
+
+
+
+Console.ReadKey();
+
+//class PersonManager
+//{
+//    // implemented operation
+//    public void Add()
+//    {
+//        Console.WriteLine("Eklendi");
+//    }
+//}
+
+interface IPersonManager
+{
+    // unimplemented operation
+    void Add();
+    void Delete();
+}
+
+class CustomerManager : IPersonManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Müşteri Eklendi");
+    }
+
+    public void Delete()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class EmployeeManager : IPersonManager
+{
+    public void Add()
+    {
+        Console.WriteLine("Personel Eklendi");
+    }
+
+    public void Delete()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class ProjectManager
+{
+    public void Add(IPersonManager personManager)
+    {
+        personManager.Add();
+    }
+}
